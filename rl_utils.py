89e73e895178b853e43a3a17d6bd1513e5f65efc
Qu Wei <wqu83204@gmail.com>
2023-11-13 11:27:51 +0800

    update

--- rl_utils.py
+++ rl_utils.py
@@ -20,12 +20,12 @@ class ReplayBuffer:
         return len(self.buffer)
 
 def moving_average(a, window_size):
-    cumulative_sum = np.cumsum(np.insert(a, 0, 0)) 
+    cumulative_sum = np.cumsum(np.insert(a, 0, 0))#[0,return_list] 
     middle = (cumulative_sum[window_size:] - cumulative_sum[:-window_size]) / window_size
-    r = np.arange(1, window_size-1, 2)
-    begin = np.cumsum(a[:window_size-1])[::2] / r
-    end = (np.cumsum(a[:-window_size:-1])[::2] / r)[::-1]
-    return np.concatenate((begin, middle, end))
+    r = np.arange(1, window_size-1, 2) #这行代码创建了一个等差数列 r，从1开始，以步长2递增，直到 window_size - 1
+    begin = np.cumsum(a[:window_size-1])[::2] / r #[::2] 表示取这个累积和列表的偶数索引处的元素
+    end = (np.cumsum(a[:-window_size:-1])[::2] / r)[::-1] # 取 a 列表从倒数第 window_size 个元素开始到开头的部分，这里包括了窗口大小之前的所有元素。
+    return np.concatenate((begin, middle, end)) 
 
 def train_on_policy_agent(env, agent, num_episodes):
     return_list = []
@@ -39,6 +39,7 @@ def train_on_policy_agent(env, agent, num_episodes):
                 while not done:
                     action = agent.take_action(state)
                     next_state, reward, done, _ = env.step(action)
+                    env.render()
                     transition_dict['states'].append(state)
                     transition_dict['actions'].append(action)
                     transition_dict['next_states'].append(next_state)
